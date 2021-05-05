832cb9342826714740a928b936d7dce7b425ca6b
Roy Sheppard <rsheppard83@gmail.com>
2021-05-05 23:06:16 +0100

    api fix and heroku settings

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoicnNoZXBwYXJkIiwiYSI6ImNrbzVhajJieDFyNWUycnBnbm0waGF0OTYifQ.1oe3tjwcHrJe8is0Pit-bw&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
