04dfad1070dc001c156d5a62bdd55dfd65a9dd76
Roy Sheppard <rsheppard83@gmail.com>
2021-05-06 19:49:15 +0100

    added precipitation to weather information.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The current precipitation is ${body.current.precip}.`)
+            callback(
+                undefined, 
+                `${body.current.weather_descriptions}.
+                 It is currently ${body.current.temperature} degrees out.
+                 It feels like ${body.current.feelslike} degrees out.
+                 The current precipitation is ${body.current.precip}.
+                `)
         }
     })
 }
